e5eba1163746a8ba8ec1146b00f45e4ea0a3c0d4
Kevin Fabio Ramos L <kramosl@unal.edu.co>
2023-03-06 01:20:51 -0500

    :bug: correccion nombres tablas

--- back/src/routes/municipios.js
+++ back/src/routes/municipios.js
@@ -4,7 +4,7 @@ const pool = require('../database');
 
 router.get('/', (req, res) => {
     //realiza una consulta a la base de datos sql
-    pool.query('SELECT * FROM municipios', (err, rows) => {
+    pool.query('SELECT * FROM MUNICIPIOS', (err, rows) => {
         if (err) {
            return res.json(err);
         }
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
     const newMunicipio = {
         nombre
     };
-    await pool.query('INSERT INTO municipios set ?', [newMunicipio]);
+    await pool.query('INSERT INTO MUNICIPIOS set ?', [newMunicipio]);
     return res.status(200).json({ message: 'Municipio saved' });
 });
 
@@ -30,13 +30,13 @@ router.put('/:id', async (req, res) => {
     const newMunicipio = {
         nombre
     };
-    await pool.query('UPDATE municipios set ? WHERE id = ?', [newMunicipio, id]);
+    await pool.query('UPDATE MUNICIPIOS set ? WHERE id = ?', [newMunicipio, id]);
     return res.status(200).json({ message: 'Municipio updated' });
 });
 
 router.delete('/:id', async (req, res) => {
     const { id } = req.params;
-    await pool.query('DELETE FROM municipios WHERE id = ?', [id]);
+    await pool.query('DELETE FROM MUNICIPIOS WHERE id = ?', [id]);
     return res.status(200).json({ message: 'Municipio deleted' });
 });
 
