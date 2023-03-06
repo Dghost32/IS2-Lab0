c03ec8481f6e9660177446a4e63b2f0d1712c601
Kevin Fabio Ramos L <kramosl@unal.edu.co>
2023-03-05 23:13:30 -0500

    backend inicializacion

---
+++ back/src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+const {engine} = require('express-handlebars');
+const path = require('path');
+//initializations 
+const app=express();
+
+//settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//global variablesrs
+app.use((req, res, next) => {
+    next()
+});
+//routes
+app.use(require('./routes/index.js'));
+app.use(require('./routes/authentication.js'));
+app.use('/direcciones', require('./routes/direcciones.js'));
+//public
+app.use(express.static(path.join(__dirname, 'public')));
+//starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
