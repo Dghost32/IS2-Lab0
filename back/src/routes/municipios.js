1d55d0e63d17bbeabe050368b108249761941e61
Carlos Jimenez <cajimenezca@unal.edu.co>
2023-03-06 01:58:02 -0500

    :bug: :ambulance: endpoints municipios fixed

--- back/src/routes/municipios.js
+++ back/src/routes/municipios.js
@@ -1,48 +1,85 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../database');
+const pool = require("../database");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
     //realiza una consulta a la base de datos sql
-    pool.query('SELECT * FROM MUNICIPIOS', (err, rows) => {
+    pool.query("SELECT * FROM MUNICIPIOS", (err, rows) => {
         if (err) {
-           return res.json(err);
+            return res.json(err);
         }
         return res.json(rows);
     });
 });
 
-//post municipios database
-router.post('/', async (req, res) => {
+//post MUNICIPIOS database
+router.post("/", async (req, res) => {
     const { nombre } = req.body;
-    if(!nombre)return res.status(400).json('Falta el nombre del municipio');
+    if (!nombre) return res.status(400).json("Falta el nombre del municipio");
     const newMunicipio = {
-        nombre
+        nombre,
     };
-    await pool.query('INSERT INTO MUNICIPIOS set ?', [newMunicipio]);
-    return res.status(200).json({ message: 'Municipio saved' });
+    await pool.query("INSERT INTO MUNICIPIOS set ?", [newMunicipio]);
+    return res.status(200).json({ message: "Municipio saved" });
 });
 
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
     const { id } = req.params;
     const { nombre } = req.body;
-    if(!nombre)return res.status(400).json('Falta el nombre del municipio');
+    if (!nombre) return res.status(400).json("Falta el nombre del municipio");
     const newMunicipio = {
-        nombre
+        nombre,
     };
-    await pool.query('UPDATE MUNICIPIOS set ? WHERE id = ?', [newMunicipio, id]);
-    return res.status(200).json({ message: 'Municipio updated' });
+    await pool.query("UPDATE MUNICIPIOS set ? WHERE id = ?", [
+        newMunicipio,
+        id,
+    ]);
+    return res.status(200).json({ message: "Municipio updated" });
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
     const { id } = req.params;
-    await pool.query('DELETE FROM MUNICIPIOS WHERE id = ?', [id]);
-    return res.status(200).json({ message: 'Municipio deleted' });
+    await pool.query("DELETE FROM MUNICIPIOS WHERE id = ?", [id]);
+    return res.status(200).json({ message: "Municipio deleted" });
 });
 
+router.post("/personas", async (req, res) => {
+    const { nombre, edad } = req.body;
+    if (!nombre) return res.status(400).json("Falta el apellido de la persona");
+    const newPersona = {
+        nombre,
+        edad,
+    };
+    await pool.query("INSERT INTO personas set ?", [newPersona]);
+    return res.status(200).json({ message: "Persona saved" });
+});
 
+router.put("/personas/:id", async (req, res) => {
+    const { id } = req.params;
+    const { nombre, edad } = req.body;
+    if (!nombre) return res.status(400).json("Falta el apellido de la persona");
+    const newPersona = {
+        nombre,
+        edad,
+    };
+    await pool.query("UPDATE personas set ? WHERE id = ?", [newPersona, id]);
+    return res.status(200).json({ message: "Persona updated" });
+});
 
+router.delete("/personas/:id", async (req, res) => {
+    const { id } = req.params;
+    await pool.query("DELETE FROM personas WHERE id = ?", [id]);
+    return res.status(200).json({ message: "Persona deleted" });
+});
 
-module.exports =  router;
+router.get("/personas", (req, res) => {
+    //realiza una consulta a la base de datos sql
+    pool.query("SELECT * FROM personas", (err, rows) => {
+        if (err) {
+            return res.json(err);
+        }
+        return res.json(rows);
+    });
+});
 
-
+module.exports = router;
