cb0da92e88de4f920d4bf639ad010a3456641ffd
Kevin Fabio Ramos L <kramosl@unal.edu.co>
2023-03-06 01:34:40 -0500

    :sparkles: endpoints persona agregados

--- back/src/routes/municipios.js
+++ back/src/routes/municipios.js
@@ -39,6 +39,43 @@ router.delete('/:id', async (req, res) => {
     await pool.query('DELETE FROM MUNICIPIOS WHERE id = ?', [id]);
     return res.status(200).json({ message: 'Municipio deleted' });
 });
+router.post('/personas', async (req, res) => {
+    const { nombre, edad } = req.body;
+    if(!nombre)return res.status(400).json('Falta el apellido de la persona');
+    const newPersona= {
+        nombre ,
+        edad
+    };
+    await pool.query('INSERT INTO personas set ?', [newPersona]);
+    return res.status(200).json({ message: 'Persona saved' });
+});
+router.put('/personas/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nombre, edad } = req.body;
+    if(!nombre)return res.status(400).json('Falta el apellido de la persona');
+    const newPersona = {
+        nombre,
+        edad
+    };
+    await pool.query('UPDATE personas set ? WHERE id = ?', [newPersona, id]);
+    return res.status(200).json({ message: 'Persona updated' });
+});
+router.delete('/personas/:id', async (req, res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM personas WHERE id = ?', [id]);
+    return res.status(200).json({ message: 'Persona deleted' });
+});
+router.get('/personas', (req, res) => {
+    //realiza una consulta a la base de datos sql
+    pool.query('SELECT * FROM personas', (err, rows) => {
+        if (err) {
+           return res.json(err);
+        }
+        return res.json(rows);
+    });
+});
+
+
 
 
 module.exports =  router;
